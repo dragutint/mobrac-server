05cb4e51858b77de10c00ce52284f6c53ffc85ae
Dragutin Todorovic <dragutin.todorovic@rgc.rs>
2020-06-19 17:42:03 +0200

    Added JsonProperty in domain class

--- src/main/java/com/mobilno/domain/Song.java
+++ src/main/java/com/mobilno/domain/Song.java
@@ -1,5 +1,6 @@
 package com.mobilno.domain;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -14,11 +15,15 @@ import java.io.Serializable;
 @Table(name = "songs")
 public class Song implements Serializable {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
+    @JsonProperty("_id")
     private Integer id;
+
     @Column(name = "song_name")
+    @JsonProperty("song_name")
     private String name;
+
     @Column(name = "artist")
     private String artist;
 }
